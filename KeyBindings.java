6faab175eea5c9134d1ac09bfe0a47bf8a428c68
alexTung523 <alextungusa@gmail.com>
2019-11-10 06:59:42 -0800

    game rules

--- KeyBindings.java
+++ KeyBindings.java
@@ -2,10 +2,11 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class KeyBindings extends JFrame {
-    private int x = 25;
-    private int y = 25;
 
+public class KeyBindings extends JFrame {
+    private int x = 27;
+    private int y = 27;
+    private boolean gate = false;
     private DrawPanel drawPanel = new DrawPanel();
 
     private KeyBindings(){
@@ -60,16 +61,17 @@ public class KeyBindings extends JFrame {
 
 
         private void map1(Graphics g) {
-            boolean x = true;
-            g.fillRect(0, 0, RECT_X, 600);
-            if (x) {
-                g.fillRect(225, 225, 25, 50);
-            }
+
+
+            g.fillRect(0, 0, 25, 600);
+
+
+            g.fillRect(225, 250, 25, 25);
+
             g.fillRect(RECT_X + 25, 0, RECT_X, 250);
             g.fillRect(0, 275, 175, 175);
             g.fillRect(0, 475, 175, 200);
             g.fillRect(0, 550, 250, 200);
-
             g.fillRect(75, 0, 175, 250);
             g.fillRect(200, 275, 50, 400);
 
@@ -79,15 +81,94 @@ public class KeyBindings extends JFrame {
             super.paintComponent(g);
             g.setColor(Color.BLACK);
             map1(g);
+            g.setColor(Color.ORANGE);
+            g.fillRect(25,450,25,25);
+            g.fillRect(175,525,25,25);
             g.setColor(Color.GREEN);
             g.fillRect(x, y, 15, 15);
+
             repaint();
+
+
+
+            //randomized key
+
+            double choose = Math.random();
+
+
+            if(choose%2 == 0) {
+                if (x > 25 && x < 50 && y > 450 && y < 475) {
+
+                    gate = true;
+
+                }
+            }else {
+                if (x > 175 && x < 200 && y < 550 && y > 525) {
+
+                    gate = true;
+
+
+                }
+            }
+            if(gate == true){
+                g.clearRect(225, 250, 25, 25);
+            }
+
+            Rectangle r1 = new Rectangle(x,y,25,25);
+            Rectangle r2 = new Rectangle(0, 0, 25, 600);
+            Rectangle r3 = new Rectangle(225, 250, 25, 25);
+            Rectangle r4 = new Rectangle(60, 0, 25, 250);
+            Rectangle r5 = new Rectangle(0, 285, 175, 155);
+            Rectangle r6 = new Rectangle(0, 465, 175, 200);
+            Rectangle r7 = new Rectangle(0, 565, 250, 200);
+            Rectangle r8 = new Rectangle(65, 0, 175, 240);
+            Rectangle r9 = new Rectangle(205, 260, 45, 380);
+            if(r1.intersects(r2)) {
+                x=25;
+                y=25;
+            }
+            if(r1.intersects(r3))
+            {
+                x=25;
+                y=25;
+            }
+            if(r1.intersects(r4))
+            {
+                x=25;
+                y=25;
+            }
+            if(r1.intersects(r5))
+            {
+                x=25;
+                y=25;
+            }
+            if(r1.intersects(r6))
+            {
+                x=25;
+                y=25;
+            }
+            if(r1.intersects(r7))
+            {
+                x=25;
+                y=25;
+            }
+            if(r1.intersects(r8))
+            {
+                x=25;
+                y=25;
+            }
+            if(r1.intersects(r9))
+            {
+                x=25;
+                y=25;
+            }
         }
 
         public Dimension getPreferredSize() {
             return new Dimension(800, 600);
         }
     }
+
 
     public static void main(String[] args) {
         EventQueue.invokeLater(new Runnable(){
@@ -96,4 +177,5 @@ public class KeyBindings extends JFrame {
             }
         });
     }
+
 }
